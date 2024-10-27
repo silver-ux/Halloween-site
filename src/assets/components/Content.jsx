247fdfe8ec2b51da4a99d0213eb5ae39d9d88dff
BuildTools <unconfigured@null.spigotmc.org>
2024-10-27 21:46:57 +0900

    nothing change

--- src/assets/components/Content.jsx
+++ src/assets/components/Content.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Content = (props) => {
   return (
-    <div style={{color: "whitesmoke", marginTop: 50, backgroundColor: "rgba(0, 0, 0, 0.6)", padding:50, borderRadius: 20}}>
+    <div style={{color: "whitesmoke", marginTop: 70, backgroundColor: "rgba(0, 0, 0, 0.6)", padding:50, borderRadius: 20}}>
         <h3 style={{fontSize:35, paddingBottom:70, textDecoration:"underline", textDecorationColor:"red"}}>{props.title}</h3>
-        <p style={{lineHeight:1.8, fontSize:24, fontFamily: "Noto Sans JP, serif"}}>{props.text}</p>
+        <p style={{lineHeight:1.8, fontSize:24, fontFamily: "Noto Sans JP, serif", textAlign: 'justify'}}>{props.text}</p>
     </div>
   )
 }
--- src/assets/pages/HistoryPage.jsx
+++ src/assets/pages/HistoryPage.jsx
@@ -3,11 +3,12 @@ import '../css/history.css'
 import { Content } from '../components/Content'
 
 export const HistoryPage = () => {
+
   return (
     <div className='history'>
       <div className="wrap">
         <div className="container">
-          <Content title="Beginning" text="Halloween originated over 2000 years ago. It is said to have originated from the festival 'Samhain' held by the ancient Celts in Europe. Samhain means 'the end of summer' and was deeply rooted in the lives of the ancient Celts as a religious event to celebrate the autumn harvest and drive away evil spirits." />
+          <Content title="Beginning" text={`Halloween originated over 2000 years ago. It is said to have originated from the festival 'Samhain' held by the ancient Celts in Europe. Samhain means 'the end of summer' and was deeply rooted in the lives of the ancient Celts as a religious event to celebrate the autumn harvest and drive away evil spirits.`} />
         </div>
       </div>
     </div>
