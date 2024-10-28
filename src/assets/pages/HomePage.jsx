d90ea4ad55bbcb7edc421af4c10fcba50a65eea5
shota.h <shota@shotaMacBook-Air.local>
2024-10-28 22:32:34 +0900

    change image files

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { MainVisual } from '../components/MainVisual'
 import { PageButtons } from '../components/PageButtons'
 import { Link } from 'react-router-dom'
+import pi1 from '../../img/7784913.jpg'
+import castle from '../../img/castle.jpg'
+import monster from '../../img/ghost.jpg'
 
 export const HomePage = () => {
   return (
@@ -10,13 +13,13 @@ export const HomePage = () => {
           <MainVisual />
           <div className="container">
             <Link to="/history">
-              <PageButtons title="History" text="Writing here about a halloween history." image="src/assets/img/7784913.jpg" />
+              <PageButtons title="History" text="Writing here about a halloween history." image={pi1} />
             </Link>
             <Link>
-              <PageButtons title="Mythology" text="Writing here about Greek mythology gods. I don't know it related to Halloween." image="src/assets/img/castle.jpg" />
+              <PageButtons title="Mythology" text="Writing here about Greek mythology gods. I don't know it related to Halloween." image={castle} />
             </Link>
             <Link>
-              <PageButtons title="Monsters" text="Writing here about Halloween Monsters." image="src/assets/img/ghost.jpg" />
+              <PageButtons title="Monsters" text="Writing here about Halloween Monsters." image={monster} />
               </Link>
           </div>
         </div>
